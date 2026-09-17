272eef7e9c5347a0d40b9c028b72c41d663dde88
Lim jy <jiyunlim94@gmail.com>
2026-09-17 11:04:07 +0900

    fix: hide user greeting after logout

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,9 @@ export default async function RootLayout({ children }) {
             <Link href="/">📝 개<small>(발자,어)</small>린이의 단어장</Link>
             <AuthControls user={user} />
           </nav>
-          <h5 className="user-name">{user.email} 님 안녕하세요!</h5>
+          {user && (
+            <h5 className="user-name">{user.email} 님 안녕하세요!</h5>
+          )}
           <h1>낯선 개발 용어를 하나씩 익혀 보세요.</h1>
           <p>초보 학습자를 위한 쉬운 설명과 짧은 코드 예시를 제공합니다.</p>
         </header>
